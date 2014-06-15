3d99f7828228258aac42ff99678ad4b4e3df8ddd
Daniel Huhn <daniel_huhn@outlook.com>
2014-06-15 17:12:13 +0200

    added log entry if invalid http param is send

--- CanBusServer/server.py
+++ CanBusServer/server.py
@@ -1,10 +1,14 @@
 # seriell einlesen ohne thread
 # UNGETESTET: das wegwerfen von empfangenen zeilen, die mit ":" beginnen!
 
+import logging
+
 from flask import Flask, request, abort
-app = Flask(__name__)
 
 from canbus import CanBus
+
+logger = logging.getLogger(__name__)
+app = Flask(__name__)
 
 PORT = 8080
 
@@ -15,13 +19,16 @@ def set_port(client_name):
     try:
         port = int(request.args.get('port', -1))
     except TypeError:
+        logger.error("invalid port")
         abort(400)
     try:
         state = int(request.args.get('state', -1))
     except TypeError:
+        logger.error("invalid port state")
         abort(400)
     print port, state
     if port == -1 or state == -1:
+        logger.error("invalid port or state")
         abort(400)
     client = canbus.get_can_client(client_name)
     return client.set_port(port, state)
@@ -31,8 +38,10 @@ def get_analog(client_name):
     try:
         port = int(request.args.get('port', -1))
     except TypeError:
+        logger.error("invalid port")
         abort(400)
     if port == -1:
+        logger.error("invalid port")
         abort(400)
     client = canbus.get_can_client(client_name)
     return client.get_analog(port)
