26f717298c54ab7b0e20eebf7b026b9cb09ea276
Sören Wegener <wegener92@gmail.com>
2014-06-15 16:41:48 +0200

    Implemented set_port and get_analog

--- CanBusServer/server.py
+++ CanBusServer/server.py
@@ -1,26 +1,42 @@
 # seriell einlesen ohne thread
 # UNGETESTET: das wegwerfen von empfangenen zeilen, die mit ":" beginnen!
 
-from http_handler import HttpHandler
-import SocketServer
+from flask import Flask, request, abort
+app = Flask(__name__)
 
 from canbus import CanBus
 
 PORT = 8080
 
+canbus = CanBus()
+
+@app.route('/<client_name>/SetPort', methods=['POST'])
+def set_port(client_name):
+    try:
+        port = int(request.args.get('port', -1))
+    except TypeError:
+        abort(400)
+    try:
+        state = int(request.args.get('state', -1))
+    except TypeError:
+        abort(400)
+    print port, state
+    if port == -1 or state == -1:
+        abort(400)
+    client = canbus.get_can_client(client_name)
+    return client.set_port(port, state)
+
+@app.route('/<client_name>/GetAnalog', methods=['GET'])
+def get_analog(client_name):
+    try:
+        port = int(request.args.get('port', -1))
+    except TypeError:
+        abort(400)
+    if port == -1:
+        abort(400)
+    client = canbus.get_can_client(client_name)
+    return client.get_analog(port)
+
+
 if __name__ == "__main__":
-    canbus = CanBus()
-
-    httpd = SocketServer.TCPServer(("", PORT), HttpHandler)
-
-    print "serving at port", PORT
-    httpd.serve_forever()
-
-    while True:
-        altair_client = canbus.get_can_client("altair")
-
-        altair_client.set_port(5, 1)
-        altair_client.set_port(5, 0)
-
-        analog_state = altair_client.get_analog(0)
-        print 'analog state: '+analog_state
+    app.run()
